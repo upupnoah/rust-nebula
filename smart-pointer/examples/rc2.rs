9bb10b11baa98f6dc1a413680f3712cea06bad07
noya <noya@mesolabs.io>
2025-01-10 04:16:56 +0800

    feat: smart pointer

---
+++ smart-pointer/examples/rc2.rs
@@ -0,0 +1,26 @@
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+// fn main() {
+//     let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+//     let b = Cons(3, Box::new(a));
+//     let c = Cons(4, Box::new(a));
+// }
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+}
